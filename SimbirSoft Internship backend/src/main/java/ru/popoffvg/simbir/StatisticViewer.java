ff1580f0b728a4b1259609aa4bda45809d186b25
popoffvg <popoffvg@gmail.com>
2021-02-17 00:11:40 +0300

    Update StatisticViewer.java

--- SimbirSoft Internship backend/src/main/java/ru/popoffvg/simbir/StatisticViewer.java
+++ SimbirSoft Internship backend/src/main/java/ru/popoffvg/simbir/StatisticViewer.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 public class StatisticViewer {
     public void print(Map<String, Integer> statistic) {
-        statistic.forEach((k,v) -> System.out.println(k + ":" + v));
+        statistic.forEach((k,v) -> System.out.println(k.toUpperCase() + " - " + v));
     }
 }
